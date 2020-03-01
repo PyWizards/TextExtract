9bc88d7070d7c7c828850f7400dde744020983d8
Sayar Mendis <sayarmendis26@gmail.com>
2020-03-01 23:20:19 +0530

    update with unittest

--- tests/texttoimg.py
+++ tests/texttoimg.py
@@ -1,7 +1,19 @@
-# from TextExtract.TextExtractApi.TextExtract import TextExtractFunctions
+import unittest
 from TextExtractApi.TextExtract import TextExtractFunctions
 
 
-result,scale=TextExtractFunctions.image_to_string_only("MenuIcon.png",lang='eng')
-result,match=TextExtractFunctions.image_to_string_matched("MenuIcon.png",expected_text='Menu',lang='eng')
-finalresult,scaled_results=TextExtractFunctions.image_to_string_matched("MenuIcon.png",expected_text='Menu',all_results=True)
+class TestTextExtract(unittest.TestCase):
+
+    def test_image_to_string(self):
+        result, scale = TextExtractFunctions.image_to_string_only("MenuIcon.png", lang='eng')
+        self.assertEqual(result,'Menu')
+
+    def test_image_to_string_matched(self):
+        result, match = TextExtractFunctions.image_to_string_matched("MenuIcon.png", expected_text='Menu', lang='eng')
+        self.assertEqual(result,'Menu')
+
+    def test_image_to_string_matched2(self):
+        finalresult, scaled_results = TextExtractFunctions.image_to_string_matched("MenuIcon.png", expected_text='Menu',
+                                                                               all_results=True)
+        self.assertEqual(finalresult,['Menu', 100.0])
+        self.assertEqual(len(scaled_results),6)
