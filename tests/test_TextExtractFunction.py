93d0dd0eac812008667e20db97d8d379ebc78b17
Sayar Mendis <sayarmendis26@gmail.com>
2020-03-02 00:12:26 +0530

    update with unittest

---
+++ tests/test_TextExtractFunction.py
@@ -0,0 +1,20 @@
+import pytest
+from TextExtractApi.TextExtract import TextExtractFunctions
+
+
+@pytest.mark.Sanity
+def test_image_to_string():
+    result, scale = TextExtractFunctions.image_to_string_only("MenuIcon.png", lang='eng')
+    assert result=='Menu',"Test Failed : Image to string not matched"
+
+@pytest.mark.Normal
+def test_image_to_string_matched():
+    result, match = TextExtractFunctions.image_to_string_matched("MenuIcon.png", expected_text='Menu', lang='eng')
+    assert result=='Menu',"Test Failed : Image to string matched with expected"
+
+@pytest.mark.Normal
+def test_image_to_string_matched2():
+    finalresult, scaled_results = TextExtractFunctions.image_to_string_matched("MenuIcon.png", expected_text='Menu',
+                                                                           all_results=True)
+    assert finalresult==['Menu', 100.0],"Test Failed : Image to string matched with expected"
+    assert len(scaled_results)==6,"Test Failed : Image to String with all scalings"
