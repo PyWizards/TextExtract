1ae2fef8626afccba51ee9fee98c9093948c70b2
Sayar Mendis <sayarmendis26@gmail.com>
2020-03-02 00:34:14 +0530

    update with pytest

--- tests/test_TextExtractFunction.py
+++ tests/test_TextExtractFunction.py
@@ -1,16 +1,18 @@
 import pytest
 from TextExtractApi.TextExtract import TextExtractFunctions
+import os
+module=os.path.dirname(os.path.abspath(__file__))
 
 def test_image_to_string():
-    result, scale = TextExtractFunctions.image_to_string_only("MenuIcon.png", lang='eng')
+    result, scale = TextExtractFunctions.image_to_string_only(module+"/MenuIcon.png", lang='eng')
     assert result=='Menu',"Test Failed : Image to string not matched"
 
 def test_image_to_string_matched():
-    result, match = TextExtractFunctions.image_to_string_matched("MenuIcon.png", expected_text='Menu', lang='eng')
+    result, match = TextExtractFunctions.image_to_string_matched(module+"/MenuIcon.png", expected_text='Menu', lang='eng')
     assert result=='Menu',"Test Failed : Image to string matched with expected"
 
 def test_image_to_string_matched2():
-    finalresult, scaled_results = TextExtractFunctions.image_to_string_matched("MenuIcon.png", expected_text='Menu',
+    finalresult, scaled_results = TextExtractFunctions.image_to_string_matched(module+"/MenuIcon.png", expected_text='Menu',
                                                                            all_results=True)
     assert finalresult==['Menu', 100.0],"Test Failed : Image to string matched with expected"
     assert len(scaled_results)==6,"Test Failed : Image to String with all scalings"
