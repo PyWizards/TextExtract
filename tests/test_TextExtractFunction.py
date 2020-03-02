21a0f8c9419bd77a36ff9d7170e76b3fe9346120
sayar mendis <sayarmendis26@gmail.com>
2020-03-02 14:41:21 +0530

    Update test_TextExtractFunction.py

--- tests/test_TextExtractFunction.py
+++ tests/test_TextExtractFunction.py
@@ -4,15 +4,18 @@ import os
 module=os.path.dirname(os.path.abspath(__file__))
 
 def test_image_to_string():
-    result, scale = TextExtractFunctions.image_to_string_only(module+"/MenuIcon.png", lang='eng')
+    image=os.path.abspath(module+"/MenuIcon.png")
+    result, scale = TextExtractFunctions.image_to_string_only(image, lang='eng')
     assert result=='Menu',"Test Failed : Image to string not matched"
 
 def test_image_to_string_matched():
-    result, match = TextExtractFunctions.image_to_string_matched(module+"/MenuIcon.png", expected_text='Menu', lang='eng')
+    image=os.path.abspath(module+"/MenuIcon.png")
+    result, match = TextExtractFunctions.image_to_string_matched(image, expected_text='Menu', lang='eng')
     assert result=='Menu',"Test Failed : Image to string matched with expected"
 
 def test_image_to_string_matched2():
-    finalresult, scaled_results = TextExtractFunctions.image_to_string_matched(module+"/MenuIcon.png", expected_text='Menu',
+    image=os.path.abspath(module+"/MenuIcon.png")
+    finalresult, scaled_results = TextExtractFunctions.image_to_string_matched(image, expected_text='Menu',
                                                                            all_results=True)
     assert finalresult==['Menu', 100.0],"Test Failed : Image to string matched with expected"
     assert len(scaled_results)==6,"Test Failed : Image to String with all scalings"
